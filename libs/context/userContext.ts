b4bc7328d0c1123c854aa43abb94d2f24964ca3a
Danilo Wanner <danilo.wanner@gmail.com>
2025-12-05 19:12:11 +0800

    Update user context

--- libs/context/userContext.ts
+++ libs/context/userContext.ts
@@ -12,7 +12,7 @@ Name: Danilo Raphael Wanner
 Nationality: Swiss
 Born: 1987
 Occupation: Director at Rapha Studio (his own company), Software Engineer, Designer
-Location: Currently in Hong Kong, planning to move to Bali at the end of 2025.
+Location: Currently in Hong Kong, moving to Bali on Feb 28, 2026.
 Partner: Kian Chong
 Technical Profile & Preferences
 
@@ -23,15 +23,17 @@ Development Values: Prioritizes fast IDE performance and fast build times.
 Primary Tools: VS Code, Google Docs.
 Personal Interests & Preferences
 
-Languages: Primarily communicates in English; also appreciates German (Hochdeutsch with Swiss spelling).
+Languages: Primarily communicates in English; also appreciates German (Hochdeutsch with Swiss spelling). Can read Jyutping quite well and speak basic Cantonese. Does not speak Mandarin.
 Diet: Vegetarian or vegan.`,
   Kian: `
 Name: Chong, Woei Jian
+Chinese Name: 張瑋健
 Western Name: Kian Chong
 Nationality: Malaysian
 Born: 1985
 Occupation: Violin teacher and director at HK Suzuki Music Institute (School in Hong Kong)
-Location: Currently in Hong Kong, planning to move to Bali at the end of 2025.
+Location: Currently in Hong Kong, moving to Bali on Feb 28, 2026.
 Partner: Danilo Raphael Wanner
+Languages: Primarily communicates in English; also understands (read/write) Malay, Cantonese, Mandarin. He knows some German and Swiss German but does not speak it fluently.
 Diet: Vegetarian or vegan.`,
 };
